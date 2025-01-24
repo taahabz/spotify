9844a45fbcf8c11cd518254d05f83683ad3f51e8
taahabz <taahabz@gmail.com>
2025-01-24 23:05:54 +0500

    Update app_theme.dart

---
+++ lib/core/configs/theme/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/core/configs/theme/app_colors.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.lightBackground,
+    
+  );
+}
